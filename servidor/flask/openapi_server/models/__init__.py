e0500dec5a01302114a3cb98f811ff2a0f39de9d
Ricardo Ardissone <ricardo.ardissone@gmail.com>
2021-04-27 15:45:08 -0300

    servidor flask openapi generator

---
+++ servidor/flask/openapi_server/models/__init__.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from openapi_server.models.cart_status import CartStatus
+from openapi_server.models.item_data import ItemData
+from openapi_server.models.login import Login
+from openapi_server.models.login_req import LoginReq
+from openapi_server.models.new_login_req import NewLoginReq
+from openapi_server.models.payment_data_req import PaymentDataReq
+from openapi_server.models.purchase_item import PurchaseItem
+from openapi_server.models.purchase_start_req import PurchaseStartReq
+from openapi_server.models.vesttype import Vesttype
