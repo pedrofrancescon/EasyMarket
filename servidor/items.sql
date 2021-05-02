cf17b265b7cc95c5786a67487f3f3fc67e17d677
rardiol <ricardo.ardissone@gmail.com>
2021-05-02 15:40:32 -0300

    Update items.sql

--- servidor/items.sql
+++ servidor/items.sql
@@ -1,7 +1,9 @@
 BEGIN TRANSACTION;
 INSERT INTO "item" VALUES 
  (315748,'Leite Vaqueiro',1004,299),
- (142877593,'Sabonete',500,300),
- (142879131,'Protetor Solar',300,200),
- (142883227,'Creme Hidratante',200,450);
+ (1344600743,'Pimenta do Reino',500,350),
+ (142879131,'Sabonete',300,200),
+ (142877593,'Protetor Solar',500,300),
+ (143945489,'Creme Hidratante',200,450),
+ (3997574617,'Adoçante',400,100);
 COMMIT;
