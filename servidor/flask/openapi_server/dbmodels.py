dd1d08bdb809cd0ac883f7cbd3e73207a950d47f
Ricardo Ardissone <ricardo.ardissone@gmail.com>
2021-05-05 08:37:36 -0300

    servidor: max one purchase per client

--- servidor/flask/openapi_server/dbmodels.py
+++ servidor/flask/openapi_server/dbmodels.py
@@ -8,13 +8,13 @@ class Client(Base):
     name = Column(String, nullable=False)
     email = Column(String, unique=True, primary_key=True)
     password = Column(String, nullable=False)
-    purchase = relationship("Purchase", back_populates="client")
+    purchase = relationship("Purchase", back_populates="client", uselist=False)
 
 
 class Purchase(Base):
     __tablename__ = 'purchase'
     id = Column(Integer, unique=True, primary_key=True)
-    client_email = Column(String, ForeignKey('client.email'), nullable=False,)
+    client_email = Column(String, ForeignKey('client.email'), nullable=False)
     cart = Column(Integer, nullable=True, unique=True)
     vest_type = Column(String, nullable=False)
     client = relationship("Client", back_populates="purchase")
